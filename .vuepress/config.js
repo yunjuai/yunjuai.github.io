62106c70ab8ecd239c9e168893d1f81401a0b582
Wang Yang <alan.wangyang@gmail.com>
2019-05-11 16:12:15 +0800

    update

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+ // 站点配置
+ plugins: [
+   [
+     'vuepress-plugin-yuque', {
+       repoUrl: 'https://www.yuque.com/guanjunqishi/hk5ic6',
+       html:true,
+     }
+   ]
+ ]
+} 
